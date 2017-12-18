798e7c2118b590cdae98c6686ca86e4030261a94
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2017-12-18 15:57:24 +0100

    jswhale
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+app.use('/', express.static(path.join(__dirname, 'static')))
+
+app.listen(3000, () => console.log('build pack app listening on port 3000!'))
